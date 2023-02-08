197b3e0b7cb06628c287ba6823cb0bc0356f4c67
tonititi <dong.vu.cong@gmail.com>
2023-02-08 15:20:11 +0700

    add comment to test

--- contracts/test/ERC20Token.sol
+++ contracts/test/ERC20Token.sol
@@ -3,6 +3,9 @@ pragma solidity >=0.6.0 <0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+
+// Add comment to test new github account
+
 contract ERC20Token is ERC20 {
     constructor() public ERC20("TestToken", "TT") {
         _mint(msg.sender, 1000000000000000);
